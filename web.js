4f43ddca2f50b2047c6ff2585ee6dd61563b2665
Marek Hrusovsky <xhruso00@gmail.com>
2013-07-14 19:47:46 +0000

    Added index.html. Umpdate web.js

--- web.js
+++ web.js
@@ -2,10 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFileSync('index.html').toString(utf8,0,buffer.length));
+var text = fs.readFileSync('index.html',utf8);
+text = text.toString('utf8');
+
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
